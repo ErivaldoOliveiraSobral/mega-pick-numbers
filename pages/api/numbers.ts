4b21a49ac585da9f53e7298c26122b7bc344240a
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2021-09-12 12:00:07 -0300

    ServerSide

---
+++ pages/api/numbers.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Array<number>>
+) {
+  const { qtd = 6 } = req.query;
+  const generatedNumbers = Array.from({length: 60}, (v, k) => ++k);
+  let responseNumbers = [];
+  for (let i = 0; i < qtd; i++) {
+    const randomIndex = Math.floor(Math.random() * generatedNumbers.length);
+      responseNumbers.push(generatedNumbers.splice(randomIndex, 1)[0]);
+  };
+  
+  res.status(200).json(responseNumbers.sort((a, b) => a - b));
+}
